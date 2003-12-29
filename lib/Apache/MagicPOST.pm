29a4aaae068c6fe3ebbce7a46125b8b0bd9268f1
Jan Algermissen <algermissen@acm.org>
2003-12-29 20:42:33 +0000

    Import of ALGER/Apache-MagicPOST-1.1 from CPAN.
    gitpan-cpan-distribution: Apache-MagicPOST
    gitpan-cpan-version:      1.1
    gitpan-cpan-path:         ALGER/Apache-MagicPOST-1.1.tar.gz
    gitpan-cpan-author:       ALGER
    gitpan-cpan-maturity:     released

--- lib/Apache/MagicPOST.pm
+++ lib/Apache/MagicPOST.pm
@@ -11,30 +11,47 @@ use AutoLoader qw(AUTOLOAD);
 our @ISA = qw(Exporter);
  
 our @EXPORT = qw();
-our $VERSION = '1.0';
+our $VERSION = '1.1';
  
 use Apache::Constants qw(:common );
 
 sub handler {
         my $r = shift;
+
         return DECLINED unless $r->method() eq 'POST' ;
 	my %params = $r->content;
+	# DEBUG
+	#$r->warn( "------------------------------------------" );
+	#$r->warn( $r->the_request() );
+	#my $href = $r->headers_in();
+	#foreach (keys %$href)
+	#{
+	#	$r->warn( "$_ -> $href->{$_}\n");
+	#}
+	#$r->warn( "------------------------------------------" );
 
-	return DECLINED unless ( exists($params{method}));
+	my $method_param_name = $r->dir_config('MagicPOSTMethodParamName');
+	
+	$method_param_name = 'method' unless ( $method_param_name );
 
-	$r->method( $params{method} );
-	delete $params{method};
+	return DECLINED unless ( exists($params{$method_param_name}));
+
+	$r->method( $params{$method_param_name} );
+	delete $params{$method_param_name};
 
 	foreach(keys %params)
 	{
 		$r->header_in( $_ => $params{$_} );
 		delete $params{$_}; 
 	}
-	$r->warn( "[info] internal redirect by Apache::MagicPOST" );
-	$r->internal_redirect( $r->uri );
-	return OK;
-}
+	$r->header_in( 'Content-Length' => undef );
+	$r->header_in( 'Content-length' => undef );
+	$r->header_in( 'Content-Type' => undef );
+	$r->header_in( 'Content-type' => undef );
 
+        return OK;
+}
+ 
 
 1;
 __END__
@@ -52,11 +69,18 @@ Apache::MagicPOST allows you to send HTTP methods to a server that
 are currently not supported by available browsers. The module,
 once installed in the PostReadRequest phase will react on the
 presence of the POST parameter 'method', alter the request
-method to the value of this parameter and redirect internally.
+method to the value of this parameter and turn all additional
+parameters into HTTP headers.
 
 In your http.conf put the following line:
 
   PerlPostReadRequestHandler Apache::MagicPOST
+
+If you need to override the default method parameter name
+'method' (because you already use 'method' in your other
+HTML forms), use the following directive:
+
+  PerlSetVar MagicPOSTMethodParamName yourParamName
 
 Make sure you put it *before* other handlers of this
 phase so it gets invoked first.
